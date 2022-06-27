f318e9ea916f71c58484c658c54b1d7ec3f9aad0
leo <94227922+abduvalee95@users.noreply.github.com>
2022-06-27 11:37:27 +0600

    echo is done

--- handlers.py
+++ handlers.py
@@ -1,8 +1,15 @@
 from main import bot, dp
 
-from aiogram.types import message
+from aiogram.types import Message
 from config import admin_id
 
 async def send_to_admin(dp):
     await bot.send_message(chat_id=admin_id, text="Бот ишлаябти")
 
+
+@dp.message_handler()
+async def echo(message: Message):
+    text = f" Салом сиз хозир {message.text} ёздингиз"
+    await bot.send_message(chat_id=message.from_user.id, text=text)
+
+    await message.answer(text=text)
